7c4b1f60b714e266c7b248f241781f95c5cd9351
Leandro <lecorvalan2@gmail.com>
2022-07-06 18:17:33 -0300

    Fetching Genres

--- two-ui/src/store/index.js
+++ two-ui/src/store/index.js
@@ -1,4 +1,6 @@
 import {configureStore,createAsyncThunk,createSlice} from "@reduxjs/toolkit";
+import { API_KEY, TMDB_BASE_URL } from "../utils/constants";
+import axios from "axios";
 
 const initialState = {
     movies: [],
@@ -6,11 +8,22 @@ const initialState = {
     genres: [],
 };
 
+export const getGenres = createAsyncThunk("two/genres", async()=> {
+    const { data:{genres} } = await axios.get(
+        `${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`
+        );
+        return genres;
+});
 
 const TwoSlice = createSlice ({
     name: "Two",
     initialState,
-    extraReducers: (builder) => {} 
+    extraReducers: (builder) => {
+        builder.addCase(getGenres.fulfilled,(state,action)=> {
+            state.genres = action.payload;
+            state.genresLoaded = true;
+        })
+    } 
 });
 
 export const store = configureStore ({
